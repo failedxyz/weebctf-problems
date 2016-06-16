f5005176ae608943a32530802255d7d4e5a5eada
Michael Zhang <me@michaelz.xyz>
2016-06-16 01:25:20 -0500

    build.sh

--- kirito-is-legit/build.sh
+++ kirito-is-legit/build.sh
@@ -1,6 +1,13 @@
+echo "Preparing Docker..."
 docker stop $(docker ps --filter "ancestor=kirito" -q)
 docker rm $(docker ps -q -f status=exited)
+
+echo "Building container for 'Kirito is Legit'..."
 docker build -t kirito $DIRECTORY
+
+echo "Running the container..."
 tmux kill-session -t kirito
 docker rm kirito
-tmux new-session -s kirito -d "docker run -it -p 6002:80 --name kirito kirito"
+tmux new-session -s kirito -d "tmux set remain-on-exit on && docker run -it -p 6002:80 --name kirito kirito"
+
+echo "Done."
