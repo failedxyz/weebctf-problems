556392651a1b1670b0feabb4b90132a4a425fdfd
Michael Zhang <me@michaelz.xyz>
2016-06-16 01:20:55 -0500

    build.sh

--- kirito-is-legit/build.sh
+++ kirito-is-legit/build.sh
@@ -1,1 +1,6 @@
-echo $DIRECTORY
+docker stop $(docker ps --filter "ancestor=kirito" -q)
+docker rm $(docker ps -q -f status=exited)
+docker build -t kirito $DIRECTORY
+tmux kill-session -t kirito
+docker rm kirito
+tmux new-session -s kirito -d "docker run -it -p 6002:80 --name kirito kirito"
