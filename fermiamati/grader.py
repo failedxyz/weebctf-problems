1a94ea0adfccc29574d6315be1d924a9b12d2978
AnnabelFaylan <annabel.faylan@gmail.com>
2016-06-28 18:30:31 -0400

    Add fermiamati

---
+++ fermiamati/grader.py
@@ -0,0 +1,6 @@
+flag = 'weeb{tmwaqzsjgipdixkxzmkfhynlzajwzyvmbvbiaxeyopgmfnbfyawhqfvysvvzrbhicsdfclhzbatbhstmzoodcryyaoskutdlgqydnsnfaxsvcpowtcxxqlrhrujpcyceeampavvcjsptjhwjsdohnyrjuqfzownkghjkcbqfcccgvzwvpnsebzylhhpdphcbkvlltbioqfkpsdphrpavspsfkmvqtttkufirfvmevebojbmiydqgxgcstzdkuxnkokbanwraixxgeethdeoudqzycsihimfrzdmetggsmzaibijuoopmjfstzbpnwnpjhdonhmvkmfuzzagmqegzkunmvxxaaaiayjedwhntexcuihjffscfipibxfyvptrgyiakrazgmkhmbvplutyustdxxdjsnjyhqaklpfmvhbxwviecxjsvoencycrzgqhmpcubxmaagkwafjlppxjrozfllikxpasnurszefzhxrsjbuogjvqh}'
+
+def grade(autogen, candidate):
+        if candidate.find(flag) >= 0:
+                return True, "Correct!"
+        return False, "Incorrect."
